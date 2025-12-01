38ccd2c9aa3b6eb61f79b73aafd2afbe5e50d047
MaxiFull10 <maxisirianidelcarril@gmail.com>
2025-11-30 21:40:05 -0300

    Update ui_device.rs for Real Racing SIN WARNINGS

--- src/frameworks/uikit/ui_device.rs
+++ src/frameworks/uikit/ui_device.rs
@@ -1,30 +1,10 @@
+#![allow(warnings)]
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 //! `UIDevice`.
-
-// --- ESTA ES LA PARTE QUE FALTABA ---
-// Definimos los módulos hijos para que el compilador sepa que existen.
-pub mod ui_accelerometer;
-pub mod ui_activity_indicator_view;
-pub mod ui_application;
-pub mod ui_color;
-// pub mod ui_device; // Este NO, porque somos nosotros mismos
-pub mod ui_event;
-pub mod ui_font;
-pub mod ui_geometry;
-pub mod ui_graphics;
-pub mod ui_image;
-pub mod ui_image_picker_controller;
-pub mod ui_nib;
-pub mod ui_responder;
-pub mod ui_screen;
-pub mod ui_touch;
-pub mod ui_view;
-pub mod ui_view_controller;
-// ------------------------------------
 
 use crate::dyld::ConstantExports;
 use crate::dyld::HostConstant;
@@ -102,14 +82,12 @@ pub const CLASSES: ClassExports = objc_classes! {
     ns_string::get_static_str(env, "iPhone OS")
 }
 
-// NSString
 - (id)systemVersion {
-    // Parche: 3.0 para compatibilidad
     ns_string::get_static_str(env, "3.0")
 }
 
 - (id)uniqueIdentifier {
-    // --- PARCHE GEMINI: UDID FIJO ---
+    // PARCHE GEMINI: UDID FIJO
     ns_string::get_static_str(env, "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
 }
 
